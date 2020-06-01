7ae74f1c14b6ddbfdfb744de9d3585737e3f0030
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2020-06-01 12:43:18 +0200

    Added tcp and git ignore file
    hehehehehe im not a gnome

---
+++ Teknologi/TCPServer/TCP/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    public class Server
+    {
+        public void DoClient(TcpClient socket)
+        {
+            using (socket)
+            {
+                NetworkStream ns = socket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true;
+
+                string line;
+                while (true)
+                {
+                    line = sr.ReadLine();
+
+                    Console.WriteLine("Client: " + line);
+                    //skriv ting der skal gøres her 
+
+                    //f.eks.
+                    //string[] str = line.Split(" ");
+                    //if (str[0].ToLower() == "gennemsnit")
+                    //{
+                    //  string kmstring = sr.ReadLine();
+                    //  Console.WriteLine("Client: " + kmstring );
+                    //  sw.WriteLine("Du har indtastet  " + kmstring + " km. Indtast antal liter");
+                    //}
+                }
+
+            }
+        }
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 7000);
+            listener.Start();
+            Console.WriteLine("Server started listening");
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Console.WriteLine("Accepted a client");
+                Task.Run((() =>
+                {
+                    TcpClient tempsocket = socket;
+                    DoClient(tempsocket);
+                }));
+            }
+        }
+    }
+}
---
+++ Teknologi/TCPServer/TCPClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            TcpClient clientSocket = new TcpClient("localhost", 7000);
+
+            Stream ns = clientSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            //Client med exit command
+            Console.WriteLine("if you want to stop type exit.. du er nu klar");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message.ToLower() == "exit")
+                {
+                    sw.WriteLine(message);
+                    break;
+                }
+
+                sw.WriteLine(message);
+                string serverAnswer = sr.ReadLine();
+                Console.WriteLine("Server: " + serverAnswer);
+            }
+
+            ns.Close();
+
+            clientSocket.Close();
+
+            Environment.Exit(0);
+        }
+    }
+}
