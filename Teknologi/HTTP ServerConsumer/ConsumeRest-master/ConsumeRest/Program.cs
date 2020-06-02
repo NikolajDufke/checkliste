b14deaeb7b7cdf5be97c246cbc4c1468741d1943
SarahWaest <48202579+SarahWaest@users.noreply.github.com>
2020-06-02 10:40:02 +0200

    ConsumeRest HTTPServerConsumer Eksempel

---
+++ Teknologi/HTTP ServerConsumer/ConsumeRest-master/ConsumeRest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ModelLib.Model;
+using Newtonsoft.Json;
+
+namespace ConsumeRest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker w1 = new Worker();
+
+            w1.Start();
+            Console.ReadLine();
+        }
+    }
+
+}
